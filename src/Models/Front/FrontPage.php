b3a10ef9978a94d0a11a04d93130d8b5a8bec7d1
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-10-01 08:17:50 +0000

    Fix styling

--- src/Models/Front/FrontPage.php
+++ src/Models/Front/FrontPage.php
@@ -95,14 +95,14 @@ class FrontPage extends Page
     public function breadcrumbs(): Attribute
     {
         return new Attribute(
-            get: fn(): array => $this->getBreadcrumbs(),
+            get: fn (): array => $this->getBreadcrumbs(),
         );
     }
 
     public function url(): Attribute
     {
         return new Attribute(
-            get: fn(): array => [
+            get: fn (): array => [
                 'title' => $this->name,
                 'is_external' => false,
                 'url' => $this->route(),
