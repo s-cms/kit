dfd9ed79b15b7cbd78c3507749ec895bafbb84b9
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-11-03 19:19:42 +0000

    Fix styling

--- src/Models/Block.php
+++ src/Models/Block.php
@@ -221,7 +221,7 @@ class Block extends Model
 
     public static function getHeaderBlocks(): array
     {
-        return Block::query()->whereIn('id', app('s')->get('header_blocks', []))->get()->map(fn($block): array => [
+        return Block::query()->whereIn('id', app('s')->get('header_blocks', []))->get()->map(fn ($block): array => [
             'id' => $block->type,
             'data' => $block->transformedData(),
         ])->toArray();
@@ -229,7 +229,7 @@ class Block extends Model
 
     public static function getFooterBlocks(): array
     {
-        return Block::query()->whereIn('id', app('s')->get('footer_blocks', []))->get()->map(fn($block): array => [
+        return Block::query()->whereIn('id', app('s')->get('footer_blocks', []))->get()->map(fn ($block): array => [
             'id' => $block->type,
             'data' => $block->transformedData(),
         ])->toArray();
