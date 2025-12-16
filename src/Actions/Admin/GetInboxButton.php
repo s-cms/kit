edb428235b11eea8c9c2be04cafea135bcbc70a6
Max Boiko <12crya34@gmail.com>
2025-12-16 10:22:52 +0200

    fix closure

--- src/Actions/Admin/GetInboxButton.php
+++ src/Actions/Admin/GetInboxButton.php
@@ -14,24 +14,26 @@ class GetInboxButton
 {
     public static function run(): Closure
     {
-        $count = ContactForm::query()->where('status', ContactFormStatusesEnum::NEW)->count();
+        return function (): string {
+            $count = ContactForm::query()->where('status', ContactFormStatusesEnum::NEW)->count();
 
-        return fn (): string => Blade::render('<div wire:poll.10s>{{$action}}</div>', [
-            'action' => Action::make('contact_form')
-                ->label(__('kit::admin.inbox'))
-                ->link()
-                ->badge($count)
-                ->badgeColor($count > 0 ? 'warning' : 'gray')
-                ->extraAttributes([
-                    'class' => 'mr-2',
-                ])
-                ->icon('heroicon-o-envelope')
-                ->iconPosition(IconPosition::After)
-                ->iconSize('sm')
-                ->size('sm')
-                ->outlined()
-                ->color('gray')
-                ->url(ContactFormResource::getUrl()),
-        ]);
+            return Blade::render('<div wire:poll.10s>{{$action}}</div>', [
+                'action' => Action::make('contact_form')
+                    ->label(__('kit::admin.inbox'))
+                    ->link()
+                    ->badge($count)
+                    ->badgeColor($count > 0 ? 'warning' : 'gray')
+                    ->extraAttributes([
+                        'class' => 'mr-2',
+                    ])
+                    ->icon('heroicon-o-envelope')
+                    ->iconPosition(IconPosition::After)
+                    ->iconSize('sm')
+                    ->size('sm')
+                    ->outlined()
+                    ->color('gray')
+                    ->url(ContactFormResource::getUrl()),
+            ]);
+        };
     }
 }
